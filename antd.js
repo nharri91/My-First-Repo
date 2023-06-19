e54183cc5773026e68dbcd9f11a6cd912d59b46b
nharri91 <134834387+nharri91@users.noreply.github.com>
2023-06-20 00:10:21 +0900

    Add files via upload

---
+++ antd.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: hsl
+ * @Date: 2022-04-03 21:50:34
+ * @LastEditTime: 2022-04-03 22:58:43
+ * @FilePath: \api-coder\src\utils\antd.js
+ * @Docs: https://www.antdv.com/docs/vue/introduce-cn
+ */
+
+import Vue from 'vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.less'
+
+Vue.use(Antd)
